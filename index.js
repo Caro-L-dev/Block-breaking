368b40d336c7e046a504901244105b3a09c48ed1
Nicole <liardcar@gmail.com>
2023-11-15 11:30:36 +0100

    feat: display canvas

---
+++ index.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+const redSquare = () => {
+    ctx.beginPath();
+    ctx.rect(20, 40, 50, 50);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    ctx.closePath();
+};
+
+const greenCircle = () => {
+    ctx.beginPath();
+    ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+};
+
+const transparentRectangle = () => {
+    ctx.beginPath();
+    ctx.rect(160, 10, 100, 40);
+    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+    ctx.stroke();
+    ctx.closePath();
+};
+
+redSquare();
+greenCircle();
+transparentRectangle();
+
+
+
+
+
+
