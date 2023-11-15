901fef35c9a9217dbfd4c29ef009ca83d744a698
Nicole <liardcar@gmail.com>
2023-11-15 15:31:02 +0100

    feat: change ball color against wall

--- index.js
+++ index.js
@@ -8,32 +8,49 @@ let xDirection = 2;
 let yDirection = -2;
 
 let ballRadius = 10;
+let initialBallColor = "#0095DD";
 
 
 const drawBall = () => {
     ctx.beginPath();
     ctx.arc(xPosition, yPosition, ballRadius, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = initialBallColor;
     ctx.fill();
     ctx.closePath();
   };
 
-  const ballCollisionAgainstWall = () => {
+const getRandomHexadecimalColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let index = 0; index < 6; index++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+const changeBallColor = () => {
+    initialBallColor = getRandomHexadecimalColor();
+};
+
+
+const ballCollisionAgainstWall = () => {
     if (yPosition + yDirection < ballRadius || yPosition + yDirection > canvas.height - ballRadius) {
         yDirection = -yDirection;
+        changeBallColor();
     };
 
     if (xPosition + xDirection < ballRadius || xPosition + xDirection > canvas.width - ballRadius) {
         xDirection = -xDirection;
+        changeBallColor();
     };
-  };
+};
   
-  const ballPath = () => {
+const ballPath = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     xPosition += xDirection;
     yPosition += yDirection;
     ballCollisionAgainstWall();
-  };
+};
 
-  setInterval(ballPath, 10);
+setInterval(ballPath, 10);
