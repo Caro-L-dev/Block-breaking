8e57d22f332f8ceebd9f878b1f433f507b4544d5
Nicole <liardcar@gmail.com>
2023-11-15 15:10:40 +0100

    feat: ball collision against wall

--- index.js
+++ index.js
@@ -7,19 +7,33 @@ let yPosition = canvas.height - 30;
 let xDirection = 2;
 let yDirection = -2;
 
+let ballRadius = 10;
+
+
 const drawBall = () => {
     ctx.beginPath();
-    ctx.arc(xPosition, yPosition, 10, 0, Math.PI * 2);
+    ctx.arc(xPosition, yPosition, ballRadius, 10, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
   };
+
+  const ballCollisionAgainstWall = () => {
+    if (yPosition + yDirection < ballRadius || yPosition + yDirection > canvas.height - ballRadius) {
+        yDirection = -yDirection;
+    };
+
+    if (xPosition + xDirection < ballRadius || xPosition + xDirection > canvas.width - ballRadius) {
+        xDirection = -xDirection;
+    };
+  };
   
-  const draw = () => {
+  const ballPath = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     xPosition += xDirection;
     yPosition += yDirection;
+    ballCollisionAgainstWall();
   };
 
-  setInterval(draw, 10);
+  setInterval(ballPath, 10);
