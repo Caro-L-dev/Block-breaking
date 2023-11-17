1b42d860b5e387f7599dd458d695c6ebee9e5090
Caro-L-dev <liardcar@gmail.com>
2023-11-17 17:04:03 +0100

    chore: update named constants

--- index.js
+++ index.js
@@ -1,53 +1,65 @@
-const canvas = document.getElementById("myCanvas");
+const CANVAS_ID = "myCanvas";
+const canvas = document.getElementById(CANVAS_ID);
+// @ts-ignore
 const ctx = canvas.getContext("2d");
 
+// @ts-ignore
 let xPosition = canvas.width / 2;
+// @ts-ignore
 let yPosition = canvas.height - 30;
 
 let xDirection = 2;
 let yDirection = -2;
 
-let ballRadius = 10;
+const BALL_RADIUS = 10;
 let ballSpeed = 1;
 
-let mainColor = "#0095DD";
-let ballColor = mainColor;
-let brickColor = mainColor;
-let paddleColor = mainColor;
-let drawScoreColor = mainColor;
-let gameOverMsgColor = "#991b1b";
-let victoryMsgColor = "#16a34a";
+const MAIN_COLOR = "#0095DD";
+const GAME_OVER_MSG_COLOR = "#991b1b";
+const VICTORY_MSG_COLOR = "#16a34a";
 
-const paddleHeight = 10;
-const paddleWidth = 80;
-let paddleX = (canvas.width - paddleWidth) / 2;
+let ballColor = MAIN_COLOR;
+let brickColor = MAIN_COLOR;
+let paddleColor = MAIN_COLOR;
+let drawScoreColor = MAIN_COLOR;
+
+const PADDLE_HEIGHT = 10;
+const PADDLE_WIDTH = 80;
+// @ts-ignore
+let xPaddle = (canvas.width - PADDLE_WIDTH) / 2;
 
 let rightPressed = false;
 let leftPressed = false;
 
-const brickRowCount = 3;
-const brickColumnCount = 5;
-const brickWidth = 75;
-const brickHeight = 20;
-const brickPadding = 10;
-const brickOffsetTop = 30;
-const brickOffsetLeft = 30;
+const RIGHT_KEY = "Right";
+const LEFT_KEY = "Left";
+const ARROW_RIGHT_KEY = "ArrowRight";
+const ARROW_LEFT_KEY = "ArrowLeft";
+
+const BRICK_ROW_COUNT = 3;
+const BRICK_COLUMN_COUNT = 5;
+const BRICK_WIDTH = 75;
+const BRICK_HEIGHT = 20;
+const BRICK_PADDING = 10;
+const BRICK_OFFSET_TOP = 30;
+const BRICK_OFFSET_LEFT = 30;
 
 let interval;
 let score = 0;
 let gameOverFlag = false;
 
 let bricks = [];
-for (let column = 0; column < brickColumnCount; column++) {
+for (let column = 0; column < BRICK_COLUMN_COUNT; column++) {
   bricks[column] = [];
-  for (let row = 0; row < brickRowCount; row++) {
+  for (let row = 0; row < BRICK_ROW_COUNT; row++) {
+    // @ts-ignore
     bricks[column][row] = { xPosition: 0, yPosition: 0, isVisible: 1 };
   }
 }
 
 const drawBall = () => {
   ctx.beginPath();
-  ctx.arc(xPosition, yPosition, ballRadius, 10, 0, Math.PI * 2);
+  ctx.arc(xPosition, yPosition, BALL_RADIUS, 0, Math.PI * 2);
   ctx.fillStyle = ballColor;
   ctx.fill();
   ctx.closePath();
@@ -55,22 +67,23 @@ const drawBall = () => {
 
 const drawPaddle = () => {
   ctx.beginPath();
-  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  // @ts-ignore
+  ctx.rect(xPaddle, canvas.height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
   ctx.fillStyle = paddleColor;
   ctx.fill();
   ctx.closePath();
 };
 
 const drawBricks = () => {
-  for (let column = 0; column < brickColumnCount; column++) {
-    for (let row = 0; row < brickRowCount; row++) {
+  for (let column = 0; column < BRICK_COLUMN_COUNT; column++) {
+    for (let row = 0; row < BRICK_ROW_COUNT; row++) {
       if (bricks[column][row].isVisible) {
-        let brickX = column * (brickWidth + brickPadding) + brickOffsetLeft;
-        let brickY = row * (brickHeight + brickPadding) + brickOffsetTop;
+        let brickX = column * (BRICK_WIDTH + BRICK_PADDING) + BRICK_OFFSET_LEFT;
+        let brickY = row * (BRICK_HEIGHT + BRICK_PADDING) + BRICK_OFFSET_TOP;
         bricks[column][row].xPosition = brickX;
         bricks[column][row].yPosition = brickY;
         ctx.beginPath();
-        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.rect(brickX, brickY, BRICK_WIDTH, BRICK_HEIGHT);
         ctx.fillStyle = brickColor;
         ctx.fill();
         ctx.closePath();
@@ -86,10 +99,10 @@ const drawScore = () => {
 };
 
 const getRandomHexadecimalColor = () => {
-  const letters = "0123456789ABCDEF";
+  const HEX_LETTERS = "0123456789ABCDEF";
   let color = "#";
   for (let index = 0; index < 6; index++) {
-    color += letters[Math.floor(Math.random() * 16)];
+    color += HEX_LETTERS[Math.floor(Math.random() * 16)];
   }
   return color;
 };
@@ -117,18 +130,19 @@ const showMessage = (msg, color) => {
 
 const displayGameResultMsg = () => {
   gameOverFlag = true;
-  if (score !== brickRowCount * brickColumnCount) {
-    showMessage("Game Over", gameOverMsgColor);
+  if (score >= BRICK_ROW_COUNT * BRICK_COLUMN_COUNT) {
+    showMessage("Super, vous avez gagné !", VICTORY_MSG_COLOR);
   } else {
-    showMessage("Super, vous avez gagné !", victoryMsgColor);
+    showMessage("Game Over", GAME_OVER_MSG_COLOR);
   }
   cancelAnimationFrame(interval);
 };
 
 const checkPaddleEdgeCollisions = () => {
   if (
-    yPosition + yDirection > canvas.height - ballRadius &&
-    (xPosition < paddleX || xPosition > paddleX + paddleWidth)
+    // @ts-ignore
+    yPosition + BALL_RADIUS > canvas.height - BALL_RADIUS &&
+    (xPosition < xPaddle || xPosition > xPaddle + PADDLE_WIDTH)
   ) {
     xDirection = -xDirection;
   } else {
@@ -139,16 +153,18 @@ const checkPaddleEdgeCollisions = () => {
 
 const ballCollisionAgainstWall = () => {
   if (
-    xPosition + xDirection < ballRadius ||
-    xPosition + xDirection > canvas.width - ballRadius
+    xPosition + xDirection < BALL_RADIUS ||
+    // @ts-ignore
+    xPosition + xDirection > canvas.width - BALL_RADIUS
   ) {
     xDirection = -xDirection;
   }
 
-  if (yPosition + yDirection < ballRadius) {
+  if (yPosition + yDirection < BALL_RADIUS) {
     yDirection = -yDirection;
-  } else if (yPosition + yDirection > canvas.height - ballRadius) {
-    if (xPosition > paddleX && xPosition < paddleX + paddleWidth) {
+    // @ts-ignore
+  } else if (yPosition + BALL_RADIUS > canvas.height - BALL_RADIUS) {
+    if (xPosition > xPaddle && xPosition < xPaddle + PADDLE_WIDTH) {
       checkPaddleEdgeCollisions();
     } else {
       displayGameResultMsg();
@@ -157,31 +173,31 @@ const ballCollisionAgainstWall = () => {
 };
 
 const keyPressed = (event) => {
-  if (event.key == "Right" || event.key == "ArrowRight") {
+  if (event.key == RIGHT_KEY || event.key == ARROW_RIGHT_KEY) {
     rightPressed = true;
-  } else if (event.key == "Left" || event.key == "ArrowLeft") {
+  } else if (event.key == LEFT_KEY || event.key == ARROW_LEFT_KEY) {
     leftPressed = true;
   }
 };
 
 const keyNoPressed = (event) => {
-  if (event.key == "Right" || event.key == "ArrowRight") {
+  if (event.key == RIGHT_KEY || event.key == ARROW_RIGHT_KEY) {
     rightPressed = false;
-  } else if (event.key == "Left" || event.key == "ArrowLeft") {
+  } else if (event.key == LEFT_KEY || event.key == ARROW_LEFT_KEY) {
     leftPressed = false;
   }
 };
 
 const ballCollisionAgainstBricks = () => {
-  for (let column = 0; column < brickColumnCount; column++) {
-    for (let row = 0; row < brickRowCount; row++) {
+  for (let column = 0; column < BRICK_COLUMN_COUNT; column++) {
+    for (let row = 0; row < BRICK_ROW_COUNT; row++) {
       let brick = bricks[column][row];
       if (brick.isVisible) {
         if (
           xPosition > brick.xPosition &&
-          xPosition < brick.xPosition + brickWidth &&
+          xPosition < brick.xPosition + BRICK_WIDTH &&
           yPosition > brick.yPosition &&
-          yPosition < brick.yPosition + brickHeight
+          yPosition < brick.yPosition + BRICK_HEIGHT
         ) {
           yDirection = -yDirection;
           brick.isVisible = false;
@@ -194,16 +210,19 @@ const ballCollisionAgainstBricks = () => {
 };
 
 const mouseMoveHandler = (event) => {
+  // @ts-ignore
   let relativeX = event.clientX - canvas.offsetLeft;
   if (
-    relativeX > paddleWidth / 2 &&
-    relativeX < canvas.width - paddleWidth / 2
+    relativeX > PADDLE_WIDTH / 2 &&
+    // @ts-ignore
+    relativeX < canvas.width - PADDLE_WIDTH / 2
   ) {
-    paddleX = relativeX - paddleWidth / 2;
+    xPaddle = relativeX - PADDLE_WIDTH / 2;
   }
 };
 
 const play = () => {
+  // @ts-ignore
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawBricks();
@@ -213,7 +232,7 @@ const play = () => {
   ballCollisionAgainstBricks();
 
   if (!gameOverFlag) {
-    if (score === brickRowCount * brickColumnCount) {
+    if (score >= BRICK_ROW_COUNT * BRICK_COLUMN_COUNT) {
       displayGameResultMsg();
       gameOverFlag = true;
       cancelAnimationFrame(interval);
@@ -222,14 +241,16 @@ const play = () => {
 
     if (!gameOverFlag) {
       if (rightPressed) {
-        paddleX += 7;
-        if (paddleX + paddleWidth > canvas.width) {
-          paddleX = canvas.width - paddleWidth;
+        xPaddle += 7;
+        // @ts-ignore
+        if (xPaddle + PADDLE_WIDTH > canvas.width) {
+          // @ts-ignore
+          xPaddle = canvas.width - PADDLE_WIDTH;
         }
       } else if (leftPressed) {
-        paddleX -= 7;
-        if (paddleX < 0) {
-          paddleX = 0;
+        xPaddle -= 7;
+        if (xPaddle < 0) {
+          xPaddle = 0;
         }
       }
 
@@ -238,10 +259,10 @@ const play = () => {
       interval = requestAnimationFrame(play);
     }
   }
-
-  document.addEventListener("keydown", keyPressed, false);
-  document.addEventListener("keyup", keyNoPressed, false);
-  document.addEventListener("mousemove", mouseMoveHandler, false);
 };
+
+document.addEventListener("keydown", keyPressed, false);
+document.addEventListener("keyup", keyNoPressed, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 play();
